22b4566457e4e94aa01c466ae23bb556f768ed74
Alexey <alexey.pakalo@gmail.com>
2019-07-02 18:06:54 +0300

    Added test button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import Button from 'antd/lib/button';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button type="primary">Button</Button>
       </header>
     </div>
   );
