071514fe68ec1a830484d0952a6241c2e2f5ec34
Alexey <alexey.pakalo@gmail.com>
2019-07-03 00:14:42 +0300

    Add same style

--- src/App.jsx
+++ src/App.jsx
@@ -13,19 +13,19 @@ class App extends Component {
   render() {
     return (
       <HashRouter>
-      <div>
-          <h1>Dashboard</h1>
-          <ul className="header">
-            <li><NavLink to="/">Главная</NavLink></li>
-            <li><NavLink to="/news">Новости</NavLink></li>
-            <li><NavLink to="/account">Личный кабинет</NavLink></li>
-          </ul>
+
+          <div className="menu">
+            <ul className="header">
+              <li><NavLink to="/">Главная</NavLink></li>
+              <li><NavLink to="/news">Новости</NavLink></li>
+              <li><NavLink to="/account">Личный кабинет</NavLink></li>
+            </ul>
+          </div>
           <div className="content">
             <Route exact path="/" component={Home}/>
             <Route path="/news" component={News}/>
             <Route path="/account" component={Account}/>
           </div>
-      </div>
       </HashRouter>
     );
   }
